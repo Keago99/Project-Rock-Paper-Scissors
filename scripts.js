5c8175a9a4f20c9040a3e6b0133dc6b8c6a155fd
Keago99 <keatabde@gmail.com>
2023-05-01 13:30:41 +0200

    rps coded with minimal functionality, will modify in later project.

--- scripts.js
+++ scripts.js
@@ -34,3 +34,42 @@ function playerChoice(){
         console.log("You entered an invalid option, ");
     }
 }
+
+function playRound(playerSelection, ComputerSelection){
+    if (playerSelection === "rock"){
+        switch(ComputerSelection){
+            case "rock":
+                console.log("PR, CR draw game");
+                break;
+            case "paper":
+                console.log("PR, CP computer wins!");
+                break;
+            case "scissors":
+                console.log("PR, CS player wins!");
+        }
+
+    }
+    if (playerSelection === "paper"){
+        switch(ComputerSelection){
+            case "rock":
+                console.log("PP, CR player wins!");
+                break;
+            case "paper":
+                console.log("PP, CP draw game");
+                break;
+            case "scissors":
+                console.log("PP, CS computer wins!");
+        }
+    }
+    if (playerSelection === "scissors" )
+    switch(ComputerSelection){
+        case "rock":
+            console.log(" PS, CR computer wins!");
+            break;
+        case "paper":
+            console.log(" PS, CP player wins!");
+            break;
+        case "scissors":
+            console.log(" PS, CS draw game");
+    }
+}
